2374a2179ba6c798eadf8e9cd28670b3b2b62859
yannick <yannick.dominguez@gmail.com>
2015-03-21 23:23:29 +0100

    added Flux store

---
+++ client/stores/__tests__/index.js
@@ -0,0 +1,35 @@
+jest.dontMock('../../dispatcher');
+jest.dontMock('../../constants');
+jest.dontMock('../');
+
+import Dispatcher from '../../dispatcher/';
+import Constants from '../../constants/';
+import Store from '../';
+
+describe('Store', () => {
+    describe('#getSearchResults', () => {
+        it('returns the latest search results', () => {
+            expect(Store.getSearchResults()).toEqual([]);
+
+            Dispatcher.dispatch({
+                actionType: Constants.UPDATE_SEARCH_RESULT,
+                data: [{foo: 'bar'}]
+            });
+
+            expect(Store.getSearchResults()).toEqual([{foo: 'bar'}]);
+        });
+    });
+
+    describe('#getSong', () => {
+        it('returns the song data', () => {
+            expect(Store.getSong()).toEqual({});
+
+            Dispatcher.dispatch({
+                actionType: Constants.UPDATE_SONG,
+                data: {foo: 'bar'}
+            });
+
+            expect(Store.getSong()).toEqual({foo: 'bar'});
+        });
+    });
+});
