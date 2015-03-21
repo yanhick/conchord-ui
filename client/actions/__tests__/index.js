0f8fd62bfb04fd177e5735efe6b1a35d0bd6a750
yannick <yannick.dominguez@gmail.com>
2015-03-21 23:46:09 +0100

    added Flux action

---
+++ client/actions/__tests__/index.js
@@ -0,0 +1,41 @@
+jest.dontMock('../../dispatcher');
+jest.dontMock('../../constants');
+jest.dontMock('../');
+
+import $ from 'jquery';
+
+import Dispatcher from '../../dispatcher/';
+import Constants from '../../constants/';
+import Actions from '../';
+
+describe('Store', () => {
+    describe('#getSong', () => {
+        it('fetches the song with the provided id', () => {
+
+            Actions.getSong(1);
+
+            expect($.ajax).toBeCalledWith({
+                type: 'GET',
+                url: 'api/songs/1',
+                success: jasmine.any(Function)
+            });
+        });
+    });
+
+    describe('#searchSong', () => {
+        it('search for songs with matching names', () => {
+
+            Actions.searchSong('my search query');
+
+            expect($.ajax).toBeCalledWith({
+                type: 'GET',
+                url: 'api/search',
+                data: {
+                    q: 'my search query'
+                },
+                success: jasmine.any(Function)
+            });
+        });
+    });
+});
+
