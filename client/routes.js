8be7cd3eaaf39472859f9889923872d63adfd34e
yannick <yannick.dominguez@gmail.com>
2015-03-22 22:50:39 +0100

    update routes to use new home location

--- client/routes.js
+++ client/routes.js
@@ -3,13 +3,10 @@ import Router from 'react-router';
 import { Route, DefaultRoute } from 'react-router';
 
 import App from './app';
-import SongContent from './components/song-content';
-import Search from './components/search';
-import Home from './components/home';
+import Home from './pages/home';
 
 export default (
     <Route name="app" path="/" handler={App}>
         <DefaultRoute handler={Home} />
-        <Route name="song" handler={SongContent} />
     </Route>
 );
