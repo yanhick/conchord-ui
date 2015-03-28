a7fddecc378412285776d7c02f14602a0457d4a1
yannick <yannick.dominguez@gmail.com>
2015-03-28 19:07:21 +0100

    added stubbed search api endpoint

--- server/Main.hs
+++ server/Main.hs
@@ -1,6 +1,16 @@
-{-# LANGUAGE OverloadedStrings #-}
+{-# LANGUAGE OverloadedStrings, DeriveGeneric, DefaultSignatures #-}
 import Web.Scotty
 import Network.Wai.Middleware.Static
+import Data.Aeson (ToJSON)
+import GHC.Generics
+
+data SearchResult = SearchResult {
+  id :: Int
+, title :: String
+, href :: String
+} deriving (Show, Generic)
+
+instance ToJSON SearchResult
 
 main :: IO ()
 main = scotty 3000 $ do
@@ -9,3 +19,8 @@ main = scotty 3000 $ do
 
   middleware $ staticPolicy (noDots >-> addBase "dist")
 
+  get "/api/search" $
+    json $ [
+      SearchResult 1 "my-song" "/my-song",
+      SearchResult 2 "mo other song" "my-other-song"
+    ]
