de69f3934430fae5bdf3c0a26346f75ebc9b3249
yannick <yannick.dominguez@gmail.com>
2015-04-01 17:09:27 +0200

    starting to implement chord line parsing

--- server/Parser.hs
+++ server/Parser.hs
@@ -1,5 +1,7 @@
 import Data.Char
 
+{-- Parse song metas --}
+
 data SongMetaType
   = Standard StandardSongMetaType
   | Other String
@@ -36,3 +38,45 @@ parseMeta s =
     [k, v] -> Just (SongMeta (getSongMetaType k) v)
     _ -> Nothing
 
+parseMetaList :: String -> [Maybe SongMeta]
+parseMetaList l = fmap parseMeta $ lines l
+
+{-- Parse song chords --}
+
+data ChordName = ChordName String
+
+data ChordNote
+  = Pressed Int
+  | Muted
+  | Opened
+
+data ChordNotes = ChordNotes {
+  highE :: ChordNote
+, b :: ChordNote
+, g :: ChordNote
+, d :: ChordNote
+, a :: ChordNote
+, lowE :: ChordNote
+}
+
+data Fingering = Fingering String
+data FingeringList = FingeringList [Fingering]
+
+data Chord = Chord {
+  name :: ChordName
+, notes :: ChordNotes
+, fingerings :: Maybe FingeringList
+, fret :: Maybe Int
+}
+
+parseChordName :: String -> ChordName
+parseChordName = undefined
+
+parseChordNotes :: String -> ChordNotes
+parseChordNotes = undefined
+
+parseFingeringList :: String -> FingeringList
+parseFingeringList = undefined
+
+parseChord :: String -> Chord
+parseChord = undefined
