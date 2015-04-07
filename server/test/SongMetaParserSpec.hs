9db82203afad9b3664c0a5f7860be033d2bc2d20
yannick <yannick.dominguez@gmail.com>
2015-04-07 05:29:19 +0200

    converted String to Data.Text in song meta parser

--- server/test/SongMetaParserSpec.hs
+++ server/test/SongMetaParserSpec.hs
@@ -2,20 +2,21 @@ module SongMetaParserSpec where
 
 import Test.Hspec
 import SongMetaParser
+import Data.Text
 
 spec :: Spec
 spec = do
   describe "SongMetaParser" $ do
       it "parses a song meta" $ do
-        parseSongMeta "title song" `shouldBe` [Right (SongMeta (Standard Title, "song"))]
+        parseSongMeta (pack "title song") `shouldBe` [Right (SongMeta (Standard Title, (pack "song")))]
 
       it "parses a list of song meta" $ do
-        parseSongMeta "title song\nartist name" `shouldBe`
-          [Right (SongMeta (Standard Title, "song")),
-          Right (SongMeta (Standard Artist, "name"))]
+        parseSongMeta (pack "title song\nartist name") `shouldBe`
+          [Right (SongMeta (Standard Title, (pack "song"))),
+          Right (SongMeta (Standard Artist, (pack "name")))]
 
       it "returns an error on wrong format" $ do
-        parseSongMeta "too much words" `shouldBe` [Left "wrong number of args"]
+        parseSongMeta (pack "too much words") `shouldBe` [Left "wrong number of args"]
 
 main :: IO ()
 main = hspec spec
