8d3a3db4fd32e49bb264126f53d860e43c88e36c
Yannick Dominguez <yannick.dominguez@gmail.com>
2016-04-17 16:47:25 -0700

    added node server boilerplate

--- server/Main.purs
+++ server/Main.purs
@@ -1,8 +1,21 @@
 module Main where
 
 import Prelude
-import Control.Monad.Eff.Console (print, CONSOLE)
+import Control.Monad.Eff.Console (print, log, CONSOLE)
 import Control.Monad.Eff (Eff())
+import Control.Monad.Eff.Class (liftEff)
+import Node.Express.App (App(), listenHttp, get)
+import Node.Express.Types (EXPRESS)
+import Node.Express.Response (send)
+import Node.HTTP (Server())
 
-main :: forall eff. Eff (console :: CONSOLE | eff) Unit
-main = print "Hello"
+main :: forall eff. Eff (console :: CONSOLE, express :: EXPRESS | eff) Server
+main = do
+    listenHttp appSetup 8080 \_ ->
+        log $ "listening on " <> show 8080
+
+appSetup :: forall e. App (console :: CONSOLE | e)
+appSetup = do
+    liftEff $ log "Setting up"
+    get "/" (send "hello")
+
