a9c8d592d59a597af9b93d109c7e793cab326f3b
Yannick Dominguez <yannick.dominguez@gmail.com>
2016-04-22 20:39:39 -0700

    finsished file server code

--- server/Main.purs
+++ server/Main.purs
@@ -1,13 +1,15 @@
 module Main where
 
 import Prelude
-import Control.Monad.Eff.Console (print, log, CONSOLE)
+import Data.Maybe (maybe)
+import Control.Monad.Eff.Console (log, CONSOLE)
 import Control.Monad.Eff (Eff())
 import Control.Monad.Eff.Class (liftEff)
 import Node.Express.App (App(), listenHttp, get)
 import Node.Express.Types (EXPRESS)
 import Node.Express.Handler (Handler())
-import Node.Express.Response (send, setResponseHeader)
+import Node.Express.Request (getRouteParam)
+import Node.Express.Response (send, sendJson, sendFile, setResponseHeader)
 import Node.HTTP (Server())
 
 main :: forall eff. Eff (console :: CONSOLE, express :: EXPRESS | eff) Server
@@ -15,8 +17,13 @@ main = do
     listenHttp appSetup 8080 \_ ->
         log $ "listening on " <> show 8080
 
+type Detail = { id :: Int, title :: String, desc :: String }
+
 results :: String
 results = "[{\"id\": 0, \"title\": \"first\", \"desc\": \"this is the first element\"}]"
+
+getDetails :: Int -> Detail
+getDetails id = { id: 0, title: "", desc: "detail for:" <> show id }
 
 details :: String
 details = "{\"id\": 0, \"title\": \"first\", \"desc\": \"this is the first element\"}"
@@ -29,6 +36,13 @@ appSetup = do
     liftEff $ log "Setting up"
     get "/results" resultsHandler
     get "/details" detailsHandler
+    get "/:file"   fileHandler
+    get "/"   fileHandler
+
+fileHandler :: forall e. Handler e
+fileHandler = do
+    fileName <- getRouteParam "file"
+    sendFile $ maybe "index.html" id fileName
 
 resultsHandler :: forall e. Handler e
 resultsHandler = do
@@ -38,5 +52,6 @@ resultsHandler = do
 detailsHandler :: forall e. Handler e
 detailsHandler = do
     setResponseHeader "Access-Control-Allow-Origin" "*"
-    send details
+    idParam <- getRouteParam "id"
+    sendJson getDetails
 
