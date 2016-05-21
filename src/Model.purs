fad037dc95d633291b4a50b63be90a45ec7f8e7e
Yannick Dominguez <yannick.dominguez@gmail.com>
2016-05-21 10:43:05 -0700

    start song spec

--- src/Model.purs
+++ src/Model.purs
@@ -1,7 +1,11 @@
 module Model where
 
-import Data.Foreign.Class (class IsForeign, readProp)
 import Prelude
+
+import Data.Foreign.Class (class IsForeign, readProp)
+import Data.Foreign (readInt, readArray, F(), ForeignError(TypeMismatch), Foreign())
+import Data.Maybe (Maybe(Nothing, Just), maybe)
+import Data.Either (Either(Left, Right), either)
 
 newtype Result = Result { id :: Int, title :: String, desc :: String }
 
@@ -17,3 +21,37 @@ type List = Array Result
 type Search = { q :: String }
 
 type Detail = Result
+
+
+type Song = {
+    id :: Int,
+    meta :: SongMeta,
+    content :: SongContent
+}
+
+type SongMeta = {
+    title :: String,
+    artist :: String,
+    album :: Maybe String,
+    year :: Year
+}
+
+type Year = Int
+
+type SongContent = Array SongSection
+
+type SongSection = {
+    id :: Int,
+    name :: SongSectionName,
+    lines :: Array SongLyric
+}
+
+type SongLyric = {
+    id :: Int,
+    text :: String,
+    chord :: SongChord
+}
+
+data SongChord = A | B | C | D | E | F | G
+
+data SongSectionName = Intro | Chorus | Verse | Outro | Bridge
