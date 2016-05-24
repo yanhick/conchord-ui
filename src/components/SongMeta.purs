78ae31be6d7b61fc763d676b15d49cd586b67355
Yannick Dominguez <yannick.dominguez@gmail.com>
2016-05-24 08:48:16 -0700

    replaced div by empty text

--- src/components/SongMeta.purs
+++ src/components/SongMeta.purs
@@ -1,7 +1,7 @@
 module SongMeta where
 
 import Data.Maybe (maybe)
-import Pux.Html (Html, div, header, h1, h2, h3, text, (#), bind)
+import Pux.Html (Html, header, h1, h2, h3, text, (#), bind)
 
 import Action (Action())
 import Model (SongMeta)
@@ -11,4 +11,4 @@ view s =
     header # do
        h1 # text s.title
        h2 # text s.artist
-       maybe (div [] []) (\a -> h3 # text a) s.album
+       maybe (text "") (\a -> h3 # text a) s.album
