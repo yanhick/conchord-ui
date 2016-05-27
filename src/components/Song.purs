1f077debe0810fb994d056740a4a02b18092471d
Yannick Dominguez <yannick.dominguez@gmail.com>
2016-05-26 19:09:59 -0700

    moved all view to a single file

--- src/components/Song.purs
+++ src/components/Song.purs
@@ -1,11 +1,18 @@
 module Song where
 
-import Data.Maybe (Maybe(Nothing, Just))
-import Pux.Html (Html, div, text, (#), bind)
-import Model (Song)
-import Action (Action())
-import SongMeta as Sm
-import SongContent as Sc
+import Prelude (($), (<$>), show, const)
+
+import Data.Maybe (Maybe(Nothing, Just), maybe, fromMaybe)
+
+import Pux.Html (Html, section, div, p, text, header, article
+                , h1, h2, h3, span, b, nav, li, button
+                , (#), (!), bind)
+import Pux.CSS (style, px, fontSize)
+import Pux.Router (link)
+import Pux.Html.Events (onClick)
+
+import Model (Song, SongMeta, SongContent, SongSection, SongLyric)
+import Action (Action(UIAction), UIAction(Increment, Decrement))
 import Header as H
 
 view :: Maybe Song -> Number -> Html Action
@@ -13,5 +20,40 @@ view Nothing _ = div # text "No song"
 view (Just s) fontSize =
     div # do
         H.view
-        Sm.view s.meta
-        Sc.view s.content fontSize
+        songMeta s.meta
+        songContent s.content fontSize
+
+songMeta :: SongMeta -> Html Action
+songMeta s =
+    header # do
+       h1 # text s.title
+       h2 # text s.artist
+       maybe (text "") (\a -> h3 # text a) s.album
+
+songContent :: SongContent -> Number -> Html Action
+songContent s fs = article [ style $ fontSize (px fs) ] (songSection <$> s)
+
+songSection :: SongSection -> Html Action
+songSection s =
+    section # do
+        h2 # text (show s.name)
+        p [] (songLyric <$> s.lyrics)
+
+songLyric :: SongLyric -> Html Action
+songLyric l =
+    span # do
+        b # text (show l.chord)
+        text $ fromMaybe "" l.text
+
+header_:: Html Action
+header_ =
+    header # do
+        nav # do
+            link "/" [] [ text "Home" ]
+            songTextSize
+
+songTextSize :: Html Action
+songTextSize =
+    li # do
+        button ! onClick (const  (UIAction Increment)) # text "+"
+        button ! onClick (const  (UIAction Decrement)) # text "-"
