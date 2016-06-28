0bacf4e28e6759671667fe47a1b58059ab1f95e5
Yannick Dominguez <yannick.dominguez@gmail.com>
2016-06-28 08:23:50 -0700

    added root chord and modifier to parser

--- common/Parser.purs
+++ common/Parser.purs
@@ -1,8 +1,10 @@
 module Parser where
 
-import Prelude (pure, class Show, bind, ($), (>>=))
+import Prelude (pure, class Show, bind, ($), (>>=), Unit(), unit, (<>), show)
 import Data.Maybe (Maybe(Just, Nothing))
-import Data.Tuple (Tuple(Tuple))
+import Data.Either (Either())
+import Data.Tuple (Tuple(Tuple), fst, snd)
+import Data.String (take, drop)
 import Control.Monad.State.Trans (StateT, runStateT)
 import Control.Monad.State.Class (get, put)
 import Control.Monad.Writer.Trans (WriterT, runWriterT)
@@ -10,15 +12,17 @@ import Control.Monad.Except.Trans (ExceptT, runExceptT)
 import Control.Monad.Error.Class (throwError)
 import Data.Identity (Identity, runIdentity)
 
-type SongChord = {
+newtype SongChord = SongChord {
     root :: SongChordRoot,
-    quality :: SongChordQuality,
-    interval :: Maybe SongChordInterval
+    rootModifier :: Maybe SongChordRootModifier
 }
+
+instance showSongChord :: Show SongChord where
+    show (SongChord { root, rootModifier }) = show root <> show rootModifier
 
 data SongChordRoot = A | B | C | D | E | F | G
 
-data SongChordRootModifier = None | Sharp | Flat
+data SongChordRootModifier = Sharp | Flat
 
 data SongChordQuality = Major | Minor
 
@@ -26,20 +30,41 @@ data SongChordInterval = Seventh
 
 type Errors = Array String
 type Log = Array String
-type ChordParser = StateT ChordParserState (WriterT Log (ExceptT Errors Identity))
+type ChordParser = StateT ChordParserState (ExceptT Errors Identity) Unit
 
-type ChordParserState = Tuple String SongChordRoot
+type ChordParserState = Tuple String SongChord
 
-runChordParser p s = runIdentity $ runExceptT $ runWriterT $ runStateT p s
+runChordParser :: ChordParser -> ChordParserState -> Either Errors (Tuple Unit ChordParserState)
+runChordParser p s = runIdentity $ runExceptT $ runStateT p s
 
-chordRoot :: ChordParser ChordParserState
-chordRoot = get >>=
-    \(Tuple s c) ->
-        case toChordRoot s of
-          (Just r) -> do
-              put (Tuple s r)
-              pure (Tuple s c)
-          Nothing -> throwError ["Not A Valid Chord root"]
+runSongChord (SongChord c) = c
+
+chordRoot :: ChordParser
+chordRoot = do
+    (sc :: ChordParserState) <- get
+    case toChordRoot (take 1 (fst sc)) of
+        (Just r) -> do
+            put $ Tuple (drop 1 (fst sc)) (SongChord ((runSongChord $ snd sc) { root = r }))
+            pure unit
+        Nothing -> throwError ["Not A Valid Chord root"]
+
+emptyChord :: SongChord
+emptyChord = SongChord { root: A, rootModifier: Nothing }
+
+chordRootModifier :: ChordParser
+chordRootModifier = do
+    (sc :: ChordParserState) <- get
+    case toChordRootModifier (take 1 (fst sc)) of
+      (Just r) -> do
+          put $ Tuple (drop 1 (fst sc)) (SongChord ((runSongChord $ snd sc) { rootModifier = Just r }))
+          pure unit
+      Nothing -> pure unit
+
+toChordRootModifier :: String -> Maybe SongChordRootModifier
+toChordRootModifier "#" = Just Sharp
+toChordRootModifier "b" = Just Flat
+toChordRootModifier _ = Nothing
+
 
 toChordRoot :: String -> Maybe SongChordRoot
 toChordRoot "A" = Just A
@@ -59,3 +84,7 @@ instance showSongChordRoot :: Show SongChordRoot where
     show E = "E"
     show F = "F"
     show G = "G"
+
+instance showSongChordRootModifier :: Show SongChordRootModifier where
+    show Sharp = "#"
+    show Flat = "b"
