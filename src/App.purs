d42a7861307712de4fa356b7b847700d6d96c689
Yannick Dominguez <yannick.dominguez@gmail.com>
2016-07-16 11:36:55 -0700

    make the rest of the app state serializable

--- src/App.purs
+++ src/App.purs
@@ -1,7 +1,8 @@
 module App where
 
 import Data.Foreign (F)
-import Data.Argonaut (class EncodeJson, encodeJson, (:=), (~>), jsonEmptyObject, fromArray)
+import Data.Either (Either(Left, Right))
+import Data.Argonaut (class EncodeJson, encodeJson, (:=), (~>), jsonEmptyObject, jsonNull)
 
 import Model (SearchResults, Song)
 import Route (Route(HomePage))
@@ -17,18 +18,38 @@ newtype State = State {
 
 instance encodeJsonState :: EncodeJson State where
     encodeJson (State { currentPage, ui, io })
-        = jsonEmptyObject
+        = "currentPage" := encodeJson currentPage
+        ~> "ui" := encodeJson ui
+        ~> "io" := encodeJson io
+        ~> jsonEmptyObject
 
 newtype UIState = UIState {
     searchQuery :: String
 }
 
+instance encodeJsonUIState :: EncodeJson UIState where
+    encodeJson (UIState { searchQuery })
+        = "searchQuery" := searchQuery
+        ~> jsonEmptyObject
+
 data AsyncData a = Loaded (F a) | Loading | Empty
+
+instance encodeJsonAsyncData :: (EncodeJson d) => EncodeJson (AsyncData d) where
+    encodeJson (Loaded (Right d)) = encodeJson d
+    encodeJson (Loaded (Left _)) = jsonNull
+    encodeJson Loading = jsonNull
+    encodeJson Empty = jsonNull
 
 newtype IOState = IOState {
     searchResults :: AsyncData SearchResults
   , song :: AsyncData Song
 }
+
+instance encodeJsonIOState :: EncodeJson IOState where
+    encodeJson (IOState { searchResults, song })
+        = "searchResults" := encodeJson searchResults
+        ~> "song" := encodeJson song
+        ~> jsonEmptyObject
 
 init :: State
 init = State {
--- src/Route.purs
+++ src/Route.purs
@@ -1,15 +1,23 @@
 module Route where
 
-import Prelude (($), (<$>))
+import Prelude (($), (<$>), (<>), show)
 import Data.Functor ((<$))
 import Control.Alt ((<|>))
 import Control.Apply ((<*), (*>))
+
+import Data.Argonaut (class EncodeJson, (:=), (~>), fromString)
 
 import Data.Maybe (fromMaybe)
 import Pux.Router (router, lit, int, end, param)
 
 
 data Route = HomePage | SearchResultPage String | SongPage Int | NotFoundPage
+
+instance encodeJsonRoute :: EncodeJson Route where
+    encodeJson HomePage = fromString "HomePage"
+    encodeJson (SearchResultPage q) = fromString $ "SearchResultPage " <> q
+    encodeJson (SongPage id) = fromString $ "SongPage " <> show id
+    encodeJson NotFoundPage = fromString "NotFoundPage"
 
 match :: String -> Route
 match url = fromMaybe NotFoundPage $ router url $
