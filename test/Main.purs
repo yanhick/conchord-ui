f690f4c3a3b0a09da3b13fa6d96108677332ccbc
Yannick Dominguez <yannick.dominguez@gmail.com>
2016-07-21 12:23:01 -0700

    remove encode/decode test

--- test/Main.purs
+++ test/Main.purs
@@ -2,7 +2,7 @@ module Test.Main where
 
 import Prelude ((==), ($), Unit, show, (<>))
 import Control.Monad.Eff (Eff)
-import Control.Monad.Eff.Console (CONSOLE)
+import Control.Monad.Eff.Console (CONSOLE, log)
 import Control.Monad.Eff.Exception (EXCEPTION)
 import Control.Monad.Eff.Random (RANDOM)
 import Data.Foreign (F)
@@ -12,10 +12,6 @@ import Test.QuickCheck (quickCheck, (<?>))
 
 import Model (SongMeta())
 
-{--main :: forall e. Eff (console :: CONSOLE, random :: RANDOM, err :: EXCEPTION | e) Unit--}
-{--main = do--}
-    {--quickCheck \s -> result s <?> "SongMeta encode/decode not idempotent"--}
-        {--where--}
-        {--result s = case (readJSON $ show $ encodeJson s) :: F SongMeta of--}
-            {--Left _ -> false--}
-            {--Right s' -> s == s'--}
+main :: forall e. Eff (console :: CONSOLE, random :: RANDOM, err :: EXCEPTION | e) Unit
+main = do
+    log "test"
