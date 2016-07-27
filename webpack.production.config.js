19ae499cda317d829797da70f8d6b2d29da0f3a1
Yannick Dominguez <yannick.dominguez@gmail.com>
2016-07-27 08:29:00 -0700

    added webpack prod file

---
+++ webpack.production.config.js
@@ -0,0 +1,46 @@
+var path = require('path');
+var webpack = require('webpack');
+
+module.exports = {
+  entry: [ path.join(__dirname, 'support/index.js') ],
+  debug: false,
+  output: {
+    filename: 'app.js'
+  },
+  module: {
+    loaders: [
+      {
+        test: /\.purs$/,
+        loader: 'purs-loader',
+        exclude: /node_modules/,
+        query: {
+          psc: 'psa',
+          bundle: true,
+          warnings: false
+        }
+      }
+    ],
+  },
+  plugins: [
+    new webpack.DefinePlugin({
+      'process.env.NODE_ENV': JSON.stringify('production')
+    }),
+    new webpack.optimize.OccurrenceOrderPlugin(true),
+    new webpack.optimize.UglifyJsPlugin({
+      minimize: true,
+      compress: {
+        warnings: false
+      }
+    }),
+  ],
+  resolveLoader: {
+    root: path.join(__dirname, 'node_modules')
+  },
+  resolve: {
+    modulesDirectories: [
+      'node_modules',
+      'bower_components'
+    ],
+    extensions: ['', '.js', '.purs']
+  }
+};
